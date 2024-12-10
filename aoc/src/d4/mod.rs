1d52ac8e76f85f3c5f068fc86cc8f210daedb7e5
Jason Syrotuck <jasyrotuck@gmail.com>
2024-12-10 14:05:00 -0800

    d4 part 1

--- aoc/src/d4/mod.rs
+++ aoc/src/d4/mod.rs
@@ -1,52 +1,83 @@
 pub mod day4 {
-    use std::ops::Index;
 
     pub fn run(lines: Vec<String>) {
         let mut result: i32 = 0;
-        let mut p2_result: i32 = 0;
+        // let mut p2_result: i32 = 0;
 
         let char_map: Vec<Vec<char>> = lines.iter().map(|l| l.chars().collect()).collect();
 
         //establish all 8 radial directions starting clockwise
-        let up: [i8; 2] = [1, 0];
-        let up_right: [i8; 2] = [1, 1];
-        let right: [i8; 2] = [0, 1];
-        let dn_right: [i8; 2] = [-1, 1];
-        let dn: [i8; 2] = [-1, 0];
-        let dn_left: [i8; 2] = [-1, -1];
-        let left: [i8; 2] = [0, -1];
-        let up_left: [i8; 2] = [1, -1];
+        let dn: [i16; 2] = [1, 0];
+        let dn_right: [i16; 2] = [1, 1];
+        let right: [i16; 2] = [0, 1];
+        let up_right: [i16; 2] = [-1, 1];
+        let up: [i16; 2] = [-1, 0];
+        let up_left: [i16; 2] = [-1, -1];
+        let left: [i16; 2] = [0, -1];
+        let dn_left: [i16; 2] = [1, -1];
 
-        let directions = [up, up_right, right, dn_right, dn, dn_left, left, up_left];
+        let directions = [dn, dn_right, right, up_right, up, up_left, left, dn_left];
 
-        for y in 0..1 {
+        for y in 0..lines.len() {
             for x in 0..lines[y].len() {
                 if char_map[y][x] == 'X' {
                     //horizontal
-                    if x < lines[y].len() - 3
-                        && char_map[y][x..x + 4] == "XMAS".chars().collect::<Vec<char>>()
-                    {
-                        println!("Found XMAS at {},{} going right", y, x);
-                        result += 1;
-                    }
-                    if x > 3 && char_map[y][(x - 3)..x + 1] == "SAMX".chars().collect::<Vec<char>>()
-                    {
-                        println!("Found XMAS at {},{} going left", y, x);
-                        result += 1;
-                    }
-
-                    // different approach
-                    let mut substrings: [[char; 4]; 8];
-                    if char_map[y][x] == 'X' {
-                        for (x, &dir) in directions.iter().enumerate() {
-                            substrings[x] = [
-                                char_map[y][x],
-                                char_map[y + dir[0] * 1][x + dir[1] * 1],
-                                char_map[y + dir[0] * 2][x + dir[1] * 2],
-                                char_map[y + dir[0] * 3][x + dir[1] * 3],
-                            ]
+                    // if x < lines[y].len() - 3
+                    //     && char_map[y][x..x + 4] == "XMAS".chars().collect::<Vec<char>>()
+                    // {
+                    //     println!("Found XMAS at {},{} going right", y, x);
+                    //     // result += 1;
+                    // }
+                    // if x > 3 && char_map[y][(x - 3)..x + 1] == "SAMX".chars().collect::<Vec<char>>()
+                    // {
+                    //     println!("Found XMAS at {},{} going left", y, x);
+                    //     // result += 1;
+                    // }
+                }
+                // different approach, compose radial coordinates, then
+                if char_map[y][x] == 'X' {
+                    for &dir in directions.iter() {
+                        let pos_4x = x as i16 + dir[1] * 3;
+                        let pos_4y = y as i16 + dir[0] * 3;
+                        if 0 > pos_4x || pos_4x >= lines[y].len() as i16 {
+                            continue;
                         }
-                        println!("{:?}", substrings);
+
+                        if 0 > pos_4y || pos_4y >= lines.len() as i16 {
+                            continue;
+                        }
+
+                        let pos: [[usize; 2]; 4] = [
+                            [y, x],
+                            [
+                                usize::try_from(y as i16 + dir[0] * 1).unwrap(),
+                                usize::try_from(x as i16 + dir[1] * 1).unwrap(),
+                            ],
+                            [
+                                usize::try_from(y as i16 + dir[0] * 2).unwrap(),
+                                usize::try_from(x as i16 + dir[1] * 2).unwrap(),
+                            ],
+                            [
+                                usize::try_from(y as i16 + dir[0] * 3).unwrap(),
+                                usize::try_from(x as i16 + dir[1] * 3).unwrap(),
+                            ],
+                        ];
+                        // println!("{:?}", pos);
+                        //
+                        let mut substring = String::from(char_map[pos[0][0]][pos[0][1]]);
+                        substring.push(char_map[pos[1][0]][pos[1][1]]);
+                        substring.push(char_map[pos[2][0]][pos[2][1]]);
+                        substring.push(char_map[pos[3][0]][pos[3][1]]);
+                        // println!("dir_vector={:?}{:?}", dir, substring);
+                        if substring == "XMAS" {
+                            println!(
+                                "Found {} at pos{:?} in direction {:?}",
+                                substring,
+                                [y, x],
+                                dir
+                            );
+                            result += 1;
+                        }
                     }
                 }
             }
@@ -54,7 +85,7 @@ pub mod day4 {
 
         println!("Day 4, Part 1. result= {}", result);
 
-        println!("Day 4, Part 2. result= {}", p2_result);
+        // println!("Day 4, Part 2. result= {}", p2_result);
 
         println!("");
     }
